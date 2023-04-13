4c41c1f75645a124596ba6b72dd73c1b2014b32b
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2023-04-13 23:12:57 +0530

    Add files via upload

---
+++ Valid Parantheses.cpp
@@ -0,0 +1,35 @@
+//yashika vaish
+bool isValid(string s) {
+        stack<char>st;
+        for(char ch:s){
+            if(ch=='(' || ch=='{' || ch=='[')
+            {
+                st.push(ch);
+            }
+            else{
+                    if(st.empty())
+                    {
+                        return false;
+                    }
+    
+                    if(ch == ')' && st.top() == '(')
+                    {
+                        st.pop();
+                    }
+                    else if(ch == '}' && st.top() == '{')
+                    {
+                        st.pop();
+                    }
+                    else if(ch == ']' && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }    
+            }    
+            return st.empty();
+    }
+};
