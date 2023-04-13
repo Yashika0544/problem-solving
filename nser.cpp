df38f2f40fbbc1c4a81853939db78193ad3a1d8c
Yashika0544 <119301739+Yashika0544@users.noreply.github.com>
2023-04-13 23:15:13 +0530

    Update nser.cpp

--- nser.cpp
+++ nser.cpp
@@ -1,8 +1,7 @@
-#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> find_next_smaller_to_right(vector<int>& arr) {
-    vector<int> result(arr.size(), -1);
+int next_smaller_to_right(vector<int>& arr) {
+    int result(arr.size(), -1);
     stack<int> s;
     for(int i = arr.size() - 1; i >= 0; i--) {
         while(!s.empty() && s.top() >= arr[i]) {
@@ -17,12 +16,12 @@ vector<int> find_next_smaller_to_right(vector<int>& arr) {
 }
 
 int main() {
-    vector<int> arr = {3, 5, 2, 4, 6, 1};
-    vector<int> result = find_next_smaller_to_right(arr);
+    arr = {3, 5, 2, 4, 6, 1};
+    result = next_smaller_to_right(arr);
     for(int i = 0; i < result.size(); i++) {
         cout << result[i] << " ";
     }
-    cout << endl; // Output: 2 2 -1 1 1 -1
+    cout << endl;
     return 0;
 }
 
